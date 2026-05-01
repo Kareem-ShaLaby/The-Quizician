1d53314333616ff636ebec2f5c95df4e0fd5cdcd
Kareem-ShaLaby <kareemshalaby0209@gmail.com>
2026-05-01 11:34:39 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -126,7 +126,7 @@ from telegram.constants import ParseMode
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "*❤️ صلي على النبي ❤️*\n"
-        "*Created by Kareem Shalaby*\n\n"
+        "*_Created by Kareem Shalaby_*\n\n"
         "Send a question like:\n\n"
         "Question?\n"
         "a) option 1\n"
